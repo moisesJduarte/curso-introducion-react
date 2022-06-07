ca05ceb7009561da13b22fa75b125d8d7b51b6cb
moisesJduarte <gladmd19@gmail.com>
2022-06-06 21:12:23 -0400

    generando onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './TodoSearch.css';
-function TodoSearch(){
+
+function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
 
-    return (
+    return [
         <input 
             className="TodoSearch"
             placeholder="task for Todo"
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 export{TodoSearch};
