41db5ea929fbae2ea756c74d4a21359c0b98e34b
moisesJduarte <gladmd19@gmail.com>
2022-09-01 02:09:40 -0400

    listo

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -39,7 +39,7 @@ function TodoForm() {
       <textarea
         value={newTodoValue}
         onChange={onChange}
-        placeholder="Cortar la cebolla para el almuerzo"
+        placeholder="agrega algo para no olvidarlo!"
       />
       <div className="TodoForm-buttonContainer">
         <button
