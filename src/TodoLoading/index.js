c873d8f4999fc4d2c23f5f1fba3467947e0f49ee
moisesJduarte <gladmd19@gmail.com>
2022-09-03 06:13:21 -0400

    maquetacion

--- src/TodoLoading/index.js
+++ src/TodoLoading/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import './TodoLoading.css';
 
-function TodoLoading() {
-    return <p>Estamos cargando, no deseperes</p>;
+function TodoLoading( { error }) {
+    return( 
+        <div className="LoadingTodo-container">
+            <span className="LoadingTodo-completeIcon"></span>
+         <p className="LoadingTodo-text">Estamos cargando, no deseperes</p>;
+        <span className="LoadingTodo-deleteIcon"></span>
+        </div>
+    );
 }
 
 export { TodoLoading };
