ba71fe334e4e6c97180568721a22cee6e453431a
moisesJduarte <gladmd19@gmail.com>
2023-01-28 10:24:43 -0500

    update

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css';
 
+
 function Modal({ children }) {
   // ReactDom tiene este método para crear portales
   return ReactDOM.createPortal(
